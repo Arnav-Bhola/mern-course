ba0f728e38bdb7921e4b47f281964e2e3844a5b5
arnav <arnav.bholausa@gmail.com>
2024-03-24 02:02:07 -0400

    gitignore

---
+++ frontend/build/precache-manifest.5e02d0b40fb8914c83ce215e1343cd01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74c9be10649db0e99da6773ee990bddc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c945efddd992c11f2775",
+    "url": "/static/css/main.c140b25e.chunk.css"
+  },
+  {
+    "revision": "1e01e782e2034b4556af",
+    "url": "/static/js/2.2673440c.chunk.js"
+  },
+  {
+    "revision": "c945efddd992c11f2775",
+    "url": "/static/js/main.d114c97a.chunk.js"
+  },
+  {
+    "revision": "2c92c4f613996492088a",
+    "url": "/static/js/runtime-main.b99ddbb6.js"
+  }
+]);
